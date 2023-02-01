dbc1966f9cdf2b0bd7ca54aa051028dacb9347ab
khanhhoanguyen <khanhhoanguyen@10.211.55.3>
2023-02-01 12:27:40 -0700

    Converted application into a full MVC pattern

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Person;
 
 /**
  *
@@ -26,15 +27,18 @@ public class HelloWorldServlet extends HttpServlet {
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
         
-        request.setAttribute("firstname",firstname);        
-        request.setAttribute("lastname",lastname);
+
         
         if(firstname == null || firstname.equals("") || lastname == null || lastname.equals("")){
+            request.setAttribute("firstname",firstname);        
+            request.setAttribute("lastname",lastname);
             getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp")
                 .forward(request, response);
             return;
         }
         
+        Person person = new Person(firstname, lastname);
+        request.setAttribute("person", person);
   
         
         getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
